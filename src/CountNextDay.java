e85c6bbdb903abc2229da4810e35cbf5274fb3af
TinyChicken12 <Tnkien12vcu@gmail.com>
2018-10-05 16:56:15 +0700

    add files

---
+++ src/CountNextDay.java
@@ -0,0 +1,22 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class CountNextDay {
+    public static String nextDay(String dateTime) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            date = simpleDateFormat.parse(dateTime);
+        } catch (Exception ex) {
+            System.out.println("Exception " + ex);
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, 1);
+        String converted = simpleDateFormat.format(calendar.getTime());
+        return converted;
+    }
+}
